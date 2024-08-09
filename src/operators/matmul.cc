3c7ae724cc04d2bae7621c13eb04800937cb3474
chenyinheng30 <chenyinheng30@outlook.com>
2024-08-09 13:42:22 +0800

    test_matmul pass.

--- src/operators/matmul.cc
+++ src/operators/matmul.cc
@@ -1,4 +1,6 @@
 #include "operators/matmul.h"
+#include <algorithm>
+#include <cstddef>
 
 namespace infini
 {
@@ -27,7 +29,19 @@ namespace infini
         // TODO：返回经过 matmul 操作后的 shape
         // REF: https://github.com/onnx/onnx/blob/main/docs/Operators.md#gemm
         // =================================== 作业 ===================================
-        return std::nullopt;
+        auto A = inputs[0]->getDims();
+        auto B = inputs[1]->getDims();
+        auto size = A.size();
+        m = transA ? A[size-1] : A[size-2];
+        n = transB ? B[size-2] : B[size-1];
+        k = transA ? A[size-2] : A[size-1];
+        Shape output;
+        for(size_t i = 0;i < size-2;i++){
+            output.push_back(std::max(A[i], B[i]));
+        }
+        output.push_back(m);
+        output.push_back(n);
+        return vector({output});
     }
 
 } // namespace infini
