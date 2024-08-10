3b942d431abb7a69c3c563dbdd89cf248adeb40d
chenyinheng30 <chenyinheng30@outlook.com>
2024-08-10 22:01:17 +0800

    test_graph pass.

--- src/core/graph.cc
+++ src/core/graph.cc
@@ -1,8 +1,16 @@
 #include "core/graph.h"
 #include "core/blob.h"
+#include "core/common.h"
+#include "core/op_type.h"
+#include "core/ref.h"
 #include "core/runtime.h"
+#include "core/tensor.h"
+#include "operators/matmul.h"
+#include "operators/transpose.h"
 #include <algorithm>
+#include <any>
 #include <cstddef>
+#include <memory>
 #include <numeric>
 #include <queue>
 
@@ -101,6 +109,14 @@ namespace infini
         return this->sorted = true;
     }
 
+    Shape permutation(const Shape& x, const Shape& y){
+        Shape z;
+        for (auto i : y) {
+            z.push_back(x[i]);
+        }
+        return z;
+    }
+
     void GraphObj::optimize()
     {
         // =================================== 作业 ===================================
@@ -109,6 +125,78 @@ namespace infini
         // 1. 去除冗余的算子（例如，两个相邻的算子都是 transpose 算子，且做的是相反的操作，可以将其全部删除）
         // 2. 合并算子（例如，矩阵乘算子中含有属性transA、transB，如果其输入存在transpose，且对最后两个维度做交换，就可以将transpose融入到矩阵乘算子的属性中去）
         // =================================== 作业 ===================================
+        if(!sorted)
+            topo_sort();
+        for (auto p = ops.begin();p < ops.end();p++) {
+            auto op = *p;
+            if (op->getOpType() == OpType::Transpose) {
+                Operator succ = op->getSuccessors()[0];
+                if(succ->getOpType() == OpType::Transpose) {
+                    Shape p1 = std::dynamic_pointer_cast<TransposeObj>(op)->getPermute();
+                    Shape p2 = std::dynamic_pointer_cast<TransposeObj>(succ)->getPermute();
+                    if (p1 == p2) {
+                        Tensor input = op->getInputs()[0];
+                        input->removeTarget(op);
+                        Tensor output = succ->getOutput();
+                        for (auto n : output->getTargets()) {
+                            input->addTarget(n);
+                            n->removePredecessors(succ);
+                            if (auto source = input->getSource(); source) {
+                                n->addPredecessors(source);
+                                source->removeSuccessors(op);
+                                source->addSuccessors(n);
+                            }
+                            n->replaceInput(output, input);
+                        }
+                        removeTensor(op->getOutput());
+                        removeTensor(output);
+                        removeOperator(succ);
+                        removeOperator(op);
+                    }
+                }
+            }
+        }
+        for (auto p = ops.begin();p < ops.end();p++) {
+            auto op = *p;
+            if (op->getOpType() == OpType::MatMul) {
+                Operator pred = op->getPredecessors()[0];
+                if (pred->getOpType() == OpType::Transpose) {
+                    Shape p = std::dynamic_pointer_cast<TransposeObj>(pred)->getPermute();
+                    Tensor output = pred->getOutput();
+                    auto matmul = std::dynamic_pointer_cast<MatmulObj>(op);
+                    if (matmul->getInputs()[0] == output && !matmul->getTransA()) {
+                        std::swap(p[p.size()-1], p[p.size()-2]);
+                        matmul->setTransA(true);
+                    }
+                    if (matmul->getInputs()[1] == output && !matmul->getTransB()) {
+                        std::swap(p[p.size()-1], p[p.size()-2]);
+                        matmul->setTransB(true);
+                    }
+                    int i = 0, permSize = static_cast<int>(p.size());
+                    for (;i < permSize;i++) {
+                        if (p[i] != i){
+                            break;
+                        }
+                    }
+                    if (i == permSize) {
+                        int j = matmul->getInputs()[0] == output ? 0 : 1;
+                        Tensor input = pred->getInputs()[0];
+                        auto source = input->getSource();
+                        if (source) {
+                            source->removeSuccessors(pred);
+                            source->addSuccessors(matmul);
+                            op->addPredecessors(source);
+                        }
+                        input->removeTarget(pred);
+                        input->addTarget(op);
+                        op->removePredecessors(pred);
+                        op->inputs[j] = input;
+                        removeOperator(pred);
+                        removeTensor(output);
+                    }
+                }
+            }
+        }
     }
 
     Tensor GraphObj::getTensor(int fuid) const
